bc2b51f32299a1d215fe1bf27233479be5e1b4d0
ReallyCodings <164112984+ReallyCodings@users.noreply.github.com>
2024-03-19 19:44:57 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var app = (function () {
+
+    var websiteName = "Somto's Basketball Shop";
+    return {
+        getWebsiteName: function () {
+            return websiteName
+        }
+    }
+})();
